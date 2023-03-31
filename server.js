ae21cb38c44fa5f4b27dd1dea0717892b482ab06
Lautaro Minue <minue.lautaro7@gmail.com>
2023-03-31 04:21:27 -0300

    test 3

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extend:true}))
 app.use("/", router);
 app.listen(PORT, () => console.log("Server Running in Port " + PORT));
 // console.log(process.env.EMAIL_USER);
